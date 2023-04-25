a32ce67fddc7180df2d47d0e84bedec4a8858843
canon1002 <k022g0038@g.neec.ac.jp>
2023-04-25 13:31:39 +0900

    プレイヤーの弾(単発弾)を追加

---
+++ PlayerBullet.cpp
@@ -0,0 +1,45 @@
+﻿#include "PlayerBullet.h"
+#include <assert.h>
+
+/// コンストラクタ
+PlayerBullet::PlayerBullet() {}
+
+/// デストラクタ
+PlayerBullet::~PlayerBullet() {}
+
+
+/// 初期化
+void PlayerBullet::Initialize(Model* model, const Vector3& position) {
+
+	// NULLポインタチェック
+	assert(m_model);
+
+	// 受け取ったデータをメンバ変数に格納
+	m_model = model;
+	// テクスチャ読み込み
+	m_textureHandle = TextureManager::Load("white1x1.png");
+
+	// ワールドトランスフォームの初期化
+	m_worldTransform.Initialize();
+	// 引数で受け取った初期座標をセット
+	m_worldTransform.translation_ = position;   
+
+}
+
+
+/// 更新処理
+void PlayerBullet::Update() {
+
+	// 行列を計算・転送
+	m_worldTransform.UpdateMatrix();
+
+}
+
+
+/// 描画処理
+void PlayerBullet::Draw(const ViewProjection& viewProjection) {
+
+	// モデルの描画
+	m_model->Draw(m_worldTransform, viewProjection, m_textureHandle);
+
+}
---
+++ PlayerBullet.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// 自キャラの弾
+/// </summary>
+class PlayerBullet {
+
+public: // メンバ関数
+
+	PlayerBullet();
+	~PlayerBullet();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	void Initialize(Model* model,const Vector3& position);
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+	
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw(const ViewProjection& viewProjection);
+
+private: // メンバ変数
+
+	// ワールド変換データ
+	WorldTransform m_worldTransform;
+	// モデル
+	Model* m_model = nullptr;
+	// テクスチャハンドル
+	uint32_t m_textureHandle = 0u;
+
+};
+
